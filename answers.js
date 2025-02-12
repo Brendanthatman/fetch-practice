3f63cf45bf9d66efb7cfba8e0ab302ffab3e9b73
Brendan Conard <117928315+Brendanthatman@users.noreply.github.com>
2025-02-12 17:09:51 +0000

    completed in-class mini-project

---
+++ answers.js
@@ -0,0 +1,8 @@
+let challenge1 = 238900 / 2159;
+console.log(challenge1);
+
+let challenge2 = ((52089 - 51316) / 51316) * 100;
+console.log(challenge2);
+
+let challenge3 = 8 * 16;
+console.log(challenge3);
---
+++ fetch.js
@@ -0,0 +1,12 @@
+fetch('https://msu-webdev.github.io/api/start.json')
+    .then(response => response.json())
+    .then(json => console.log(json));
+fetch('https://msu-webdev.github.io/api/110.json')
+    .then(response => response.json())
+    .then(json => console.log(json));
+fetch('https://msu-webdev.github.io/api/15.json')
+    .then(response => response.json())
+    .then(json => console.log(json));
+fetch('https://msu-webdev.github.io/api/128.json')
+    .then(response => response.json())
+    .then(json => console.log(json));
